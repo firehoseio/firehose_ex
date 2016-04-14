c850821607a1c93c35223449af5ee0607716224e
Christopher Bertels <bakkdoor@flasht.de>
2016-04-14 22:30:51 +0200

    Add basic http long poll subscription logic

---
+++ lib/firehose_ex/channel.ex
@@ -0,0 +1,74 @@
+defmodule FirehoseEx.Channel do
+  @moduledoc """
+  Firehose Channel implementation
+  """
+
+  require Logger
+
+  def next_message(channel, last_sequence) do
+    {:ok, [curr_seq, messages]} = FirehoseEx.Redis.pipeline([
+      [:get, sequence_key(channel)],
+      [:lrange, list_key(channel), 0, buffer_size]
+    ])
+
+    Logger.debug "Redis.pipeline(#{sequence_key(channel)}) returned: `#{curr_seq}` and `#{inspect messages}`"
+
+    handle_next_message(channel, last_sequence, curr_seq |> parse_seq, messages)
+  end
+
+  def handle_next_message(channel, _, nil, _), do: subscribe(channel)
+
+  def handle_next_message(channel, last_seq, curr_seq, _)
+  when curr_seq - last_seq <= 0 do
+    subscribe(channel)
+  end
+
+  def handle_next_message(channel, last_seq, curr_seq, messages) do
+    diff = curr_seq - last_seq
+    if diff < buffer_size do
+      message = messages |> Enum.at(diff - 1)
+      Logger.debug "Sending old message `#{message}` and sequence `#{curr_seq}` to client directly. Client is `#{diff}` behind, at `#{last_seq}`."
+      {message, last_seq + 1}
+    else
+      [message | _] = messages
+      Firehose.logger.debug "Sending latest message `#{message}` and sequence `#{curr_seq}` to client directly."
+      {message, curr_seq}
+    end
+  end
+
+  def parse_seq(nil), do: nil
+  def parse_seq(val) when is_binary(val), do: val |> String.to_integer
+
+  @payload_delimiter "\n"
+
+  def subscribe(channel, opts \\ [timeout: :infinity]) do
+    key = channel_updates_key(channel)
+    FirehoseEx.Redis.subscribe(key, self)
+    receive do
+      {:redix_pubsub, :subscribe, ^key, nil} ->
+        receive do
+          {:redix_pubsub, :message, msg, ^key} ->
+            [^channel, sequence, message] = msg |> String.split(@payload_delimiter)
+            {message, sequence |> parse_seq}
+          after opts[:timeout] ->
+            Logger.info "Subscribe timed out for channel: #{channel} in pid: #{inspect self}"
+        end
+    end
+  end
+
+  def buffer_size do
+    Application.get_env(:firehose_ex, :channel)[:buffer_size]
+  end
+
+  def channel_updates_key(channel) do
+    FirehoseEx.Redis.key([channel, :channel_updates])
+  end
+
+  def sequence_key(channel) do
+    FirehoseEx.Redis.key([channel, :sequence])
+  end
+
+  def list_key(channel) do
+    FirehoseEx.Redis.key([channel, :list])
+  end
+end
--- lib/firehose_ex/router.ex
+++ lib/firehose_ex/router.ex
@@ -22,6 +22,38 @@ defmodule FirehoseEx.Router do
 
   get "/revision" do
     conn
-    |> send_resp(200, System.get_env("REVISION"))
+    |> send_resp(200, System.get_env("REVISION") || "")
   end
+
+  get "/channels@firehose" do
+    conn
+    |> send_resp(200, "TODO")
+  end
+
+  match _ do
+    conn = conn |> fetch_query_params
+    channel = conn.request_path
+    last_sequence = conn.params |> last_message_sequence
+
+    if last_sequence < 0 do
+      conn
+      |> send_resp(400, "The last_message_sequence parameter may not be less than zero")
+    else
+      {msg, curr_seq} = FirehoseEx.Channel.next_message(channel, last_sequence)
+      conn
+      |> json_response(200, %{message: msg, last_sequence: curr_seq})
+    end
+  end
+
+  def json_response(conn, status, data) do
+    conn
+    |> put_resp_content_type("text/json")
+    |> send_resp(status, Poison.encode!(data))
+  end
+
+  defp last_message_sequence(%{last_message_sequence: val}) do
+    val |> String.to_integer
+  end
+
+  defp last_message_sequence(_), do: 0
 end
